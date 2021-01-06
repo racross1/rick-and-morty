74f3f613416a457930d9c24943c462b845cd7248
Lucas Montanari <Lmonty2011@gmail.com>
2021-01-05 17:16:41 -0800

    added navbar w/ bootstrap and did some other styling with css

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
-
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#require_relative "app/views/navbar/nav.html"
